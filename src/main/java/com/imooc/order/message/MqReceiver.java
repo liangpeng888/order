9e8839df20e9cd92922ed1dd7dea942453251c11
q1006820364 <1006820364@qq.com>
2019-05-08 22:29:31 +0800

    queue 自动绑定 exchange

--- src/main/java/com/imooc/order/message/MqReceiver.java
+++ src/main/java/com/imooc/order/message/MqReceiver.java
@@ -1,7 +1,9 @@
 package com.imooc.order.message;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class MqReceiver {
 
-    //手动创建队列 1. @RabbitListener(queues = "myQueue")
-    //自动创建队列
-    @RabbitListener(queuesToDeclare = @Queue("myQueue"))
+    //1.手动创建队列 1. @RabbitListener(queues = "myQueue")
+    //2.自动创建队列 @RabbitListener(queuesToDeclare = @Queue("myQueue"))
+    //2.自动创建队列 Queue 和 Exchange绑定
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue("myQueue"),
+            exchange = @Exchange("myExchange")
+    ))
     public void process(String message){
         log.info("MqReceiver:{}",message);
     }
